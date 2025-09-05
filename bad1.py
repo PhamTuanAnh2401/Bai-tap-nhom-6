329bf99d37315021f4417385b255293904023839
PhamTuanAnh2401 <dtc2154802010036@ictu.edu.vn>
2025-09-05 08:34:41 +0700

    Tuấn Anh sửa code bad 1 thành clean

--- bad1.py
+++ bad1.py
@@ -1,12 +1,28 @@
-students = []
+class Student:
+    def __init__(self, student_id, name, major, age):
+        self.student_id = student_id
+        self.name = name
+        self.major = major
+        self.age = age
 
-def add_student(s):
-    students.append(s)
+    def __str__(self):
+        return f"{self.student_id} | {self.name} | {self.major} | {self.age}"
 
-def show_students():
-    for s in students:
-        print(s)
 
-add_student("1|Nam|CNTT|20")
-add_student("2|Lan|KinhTe|21")
-show_students()
+class StudentManager:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+    def show_students(self):
+        for student in self.students:
+            print(student)
+
+
+# Demo
+manager = StudentManager()
+manager.add_student(Student(1, "Nam", "CNTT", 20))
+manager.add_student(Student(2, "Lan", "Kinh tế", 21))
+manager.show_students()
